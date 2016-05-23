c8c458f575cd8b253fa814db12074e720d423587
Ivan Pakhomov <pahomov@bank24.int>
2016-05-23 19:22:17 +0500

    first commit

---
+++ README.md
@@ -0,0 +1,3 @@
+Курс по React, RethinkDB и Go от James Moore
+Серверная часть приложения
+http://courses.knowthen.com/courses/learn-how-to-develop-realtime-web-apps
---
+++ handlers.go
@@ -0,0 +1,43 @@
+package main
+
+import (
+    r "github.com/dancannon/gorethink"
+    "github.com/mitchellh/mapstructure"
+    "fmt"
+)
+
+func addChannel(client *Client, data interface{}) {
+    var channel Channel
+    err := mapstructure.Decode(data, &channel)
+    if err != nil {
+        client.send <- Message{"error", err.Error()}
+        return
+    }
+    go func() {
+        err = r.Table("channel").
+            Insert(channel).
+            Exec(client.session)
+        if err != nil {
+            client.send <- Message{"error", err.Error()}
+        }
+    }()
+}
+
+func subscribeChannel(client *Client, data interface{}) {
+    go func() {
+        cursor, err := r.Table("channel").
+            Changes(r.ChangesOpts{IncludeInitial: true}).
+            Run(client.session)
+        if err != nil {
+            client.send <- Message{"error", err.Error()}
+            return
+        }
+        var change r.ChangeResponse
+        for cursor.Next(&change) {
+            if change.NewValue != nil && change.OldValue == nil {
+                client.send <- Message{"channel add", change.NewValue}
+                fmt.Println("sent Channel Add message")
+            }
+        }
+    }()
+}
